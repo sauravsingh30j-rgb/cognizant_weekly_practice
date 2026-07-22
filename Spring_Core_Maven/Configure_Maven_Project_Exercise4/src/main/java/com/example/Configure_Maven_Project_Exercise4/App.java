8c8f2a9a5f4bacae1b5be075bcdf5e363c4e2710
sauravsingh30j-rgb <saurav.singh.30j@gmail.com>
2026-07-22 18:18:55 +0530

    Spring zcore completed

---
+++ Spring_Core_Maven/Configure_Maven_Project_Exercise4/src/main/java/com/example/Configure_Maven_Project_Exercise4/App.java
@@ -0,0 +1,13 @@
+package com.example.Configure_Maven_Project_Exercise4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
